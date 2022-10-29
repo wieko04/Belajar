b2aaac8bb00e1e37de9443fc7bae0c39b977e6a2
wieko04 <wieko04@gmail.com>
2022-10-29 17:30:48 +0700

    test

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $host   ="localhost";
+    $user   ="root";
+    $pass   ="";
+    $db     ="db_paswa";
+
+    $mysqli = mysqli_connect(
+        $host,
+        $user,
+        $pass,
+        $db,
+    )
+?>
